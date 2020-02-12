58f0eb06a8e942ab0c5736cef027803e0ac7db72
Mike Fischer <mikefischer99@gmail.com>
2020-02-12 12:35:59 -0700

    Fix for PrusaSlicer 2.2x
    PrusaSlicer no longer zeros the extruder location on each layer.

--- PrusaSlicer/filament_change.pl
+++ PrusaSlicer/filament_change.pl
@@ -7,6 +7,8 @@ use strict;
 use warnings;
 
 my $g1z = '';     # used to store each "G1 Z" command encountered
+my $g1e = '';     # used to store each "G1 E" command encountered
+
 my $height = 10;  # z height to move to befor filament swap
 my $wipe_y = 0;   # y location to use for next nozzle purge
 
@@ -24,10 +26,19 @@ while (<>) {
     $height += 20;
   }
 
+  # Store any "G1 E" commands seen. The last one before an M600 will
+  # be used to return the extruder to the correct coordinate after
+  # the filament swap and purge.
+  #
+  # Example: G1 X56.590 Y22.241 E724.47683
+  if ($_ =~ /G1.+ (E.+$)/) {
+    $g1e = $1;
+  }
+
   # If the line is M600, insert custom filament change code.
   if (rindex($_, "M600", 0) == 0) {
     $wipe_y += 3;            # Move the wipe (nozzle clean) line a bit further
-    print "; Filament / color change\n";
+    print "; MDF Filament / color change\n";
     print "G92 E0            ; Zero the extruded length\n";
     print "G1 E-25 F1200     ; Retract for easier filament swap\n";
     print "G0 Z$height            ; Move up a bit\n";
@@ -36,10 +47,9 @@ while (<>) {
     print "G0 Z0.15          ; Move back to layer 0 for wipe\n";
     print "G92 E0            ; Zero the extruded length\n";
     print "G1 X40 E25 F500   ; Extrude 25mm of filament in a 40mm line\n";
-    print "G92 E0            ; Zero the extruded length\n";
-    print "G1 E-5 F1000      ; Retract 5mm\n";
+    print "G1 E22 F1000      ; Retract 3mm\n";
     print "G1 X80 F4000      ; Quickly wipe away from the filament line\n";
-    print "G92 E0            ; Zero extruder - Prusa restarts at each layer\n";
+    print "G92 $g1e        ; Restore extruder position\n";
     print $g1z;              # Move back to correct Z height.
   } else {
     print;    # Copy all other lines as-is
